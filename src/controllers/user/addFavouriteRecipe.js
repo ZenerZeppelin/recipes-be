0bae5c6b8b07b85d2aa96027431696ff8b63ab02
Olivera <oliveram1996@gmail.com>
2019-12-11 16:09:44 +0100

    Authorization for adding favourite recipe added

--- src/controllers/user/addFavouriteRecipe.js
+++ src/controllers/user/addFavouriteRecipe.js
@@ -1,9 +1,29 @@
 import User from "../../models/user";
+import jwt from "jsonwebtoken";
 
 export const addFavouriteRecipe = async (req, res, next) => {
     try {
-        const { idRecipe, idUser } = req.body;
-        const user = await User.findById({ _id: idUser });
+
+        const authHeader = req.get("Authorization");
+        if (!authHeader) {
+            throw new Error("Not logged in");
+        }
+
+        const token = authHeader.split(" ")[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, "secret_key");
+        } catch (error) {
+            console.log(error);
+            throw new Error("Not authenticated");
+        }
+
+        if (!decodedToken) {
+            throw new Error("Not authenticated");
+        }
+
+        const { idRecipe } = req.body;
+        const user = await User.findById({ _id: decodedToken.id });
         if (!user) {
             throw new Error('User does not exist');
         }
@@ -11,12 +31,12 @@ export const addFavouriteRecipe = async (req, res, next) => {
         if (!favRecipes || favRecipes.length == 0) {
             const favouriteRecipes = [];
             favouriteRecipes.push(idRecipe);
-            await User.findByIdAndUpdate({ _id: idUser }, { recipes: favouriteRecipes });
+            await User.findByIdAndUpdate({ _id: decodedToken.id }, { recipes: favouriteRecipes });
             return res.status(201).json({ "message": "Recipe added successfully" });
         }
 
         favRecipes.push(idRecipe);
-        await User.findByIdAndUpdate({ _id: idUser }, { recipes: favRecipes });
+        await User.findByIdAndUpdate({ _id: decodedToken.id }, { recipes: favRecipes });
         return res.status(201).json({ "message": "Recipe added successfully" });
 
     } catch (err) {
